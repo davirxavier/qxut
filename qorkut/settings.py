f5548edde64775e953a0442f662806e419c5c464
davi_xavier <47609623+davirxavier@users.noreply.github.com>
2021-11-20 16:07:37 -0300

    Add db settings

--- qorkut/settings.py
+++ qorkut/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -87,7 +88,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+if os.getenv('ENABLE_REMOTE_DB', False):
+    DATABASES['default'].update(dj_database_url.config(conn_max_age=600))
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
